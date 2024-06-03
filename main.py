27b5b2dccd9cc4ead9a652a1d8cad9f9b0c6522b
Denis Borisov <mai.borisov.denis@gmail.com>
2024-06-03 07:30:30 +0300

    .

--- main.py
+++ main.py
@@ -60,28 +60,6 @@ async def get_current_active_user(
     return current_user
 
 
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
-
-
-@app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
-
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
 @app.post("/register")
 async def register_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)
                         ) -> auth.Token:
@@ -100,7 +78,7 @@ async def register_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends(
         )
 
 
-@app.post("/token")
+@app.post("/login")
 async def login_for_access_token(
         form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)
 ) -> auth.Token:
@@ -116,13 +94,6 @@ async def login_for_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
     return auth.Token(access_token=access_token, token_type="bearer")
-
-
-@app.get("/users/me/", response_model=auth.User)
-async def read_users_me(
-        current_user: Annotated[auth.User, Depends(get_current_active_user)],
-):
-    return current_user
 
 
 @app.get("/recommendations")
